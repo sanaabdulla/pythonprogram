95478e54d304e09b82e9a032e86e81485b74a02c
sanaabdulla <145667490+sanaabdulla@users.noreply.github.com>
2023-10-26 23:10:48 +0530

    Add files via upload

---
+++ 19-10-23/dict,py.py
@@ -0,0 +1,6 @@
+d={1:2,7:2,3:4,5:6,0:8}
+print("THE DICTIONARY:",d)
+sorted_d=sorted(d.items())
+print("DICTIONARY IN ASCENDING:",sorted_d)
+sorted_rev=sorted(d.items(),reverse=True)
+print("DICTIONARY IN DESCEND:",sorted_rev)
